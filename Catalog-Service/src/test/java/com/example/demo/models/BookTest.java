66c9af02fc84a1aa27409cfe6cbfcf2b98537e4f
AbhishekInfobelt <abhishek.rangari@infobelt.com>
2024-11-24 00:23:15 +0530

    minor changes and self created workflow.yml file

--- Catalog-Service/src/test/java/com/example/demo/models/BookTest.java
+++ Catalog-Service/src/test/java/com/example/demo/models/BookTest.java
@@ -1,7 +1,5 @@
 package com.example.demo.models;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
